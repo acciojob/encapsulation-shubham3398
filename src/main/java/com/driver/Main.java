f52099d76ea094a6c6d3ced3940d674a00d2493d
vshubham331998 <vshubham331998@gmail.com>
2023-08-12 13:49:04 +0530

    made correction

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,6 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        obj.name = "Shubham";
-        System.out.println(obj.name);
 
         //name has private access in com.driver.RWOnly
 
